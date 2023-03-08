cca515ac61fa0575745b3f1aac417a34ec91f145
mohamedmehdigara <garamehdi57@yahoo.com>
2023-03-08 17:15:20 +0100

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-function App() {
+const User = () => {
+  return <div>This is the user page</div>;
+};
+
+const Home = () => {
+  return <div>This is the home page</div>;
+};
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <nav>
+          <div>
+            <Link to="/">Home</Link>
+          </div>
+          <div>
+            <Link to="/user/:id">User</Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user/:id" element={<User />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
